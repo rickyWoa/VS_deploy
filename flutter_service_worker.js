9ea7fb056f5acceacc3ab239d9ee92e9335db264
rickyWoa <riccardo@woacreativecompany.com>
2024-09-18 12:40:03 +0200

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "237506ef640a1bd4e18a4a47127fca48
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "3347fbba9b672af8c9ebba8f6894a8c3",
+"flutter_bootstrap.js": "7d23e759d0493a7d662db563beac1696",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
